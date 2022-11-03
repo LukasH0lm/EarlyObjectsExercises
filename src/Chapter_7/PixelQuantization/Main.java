0d166496fa1c4d173b2f8e6bff443ef61f77244c
LukasH0lm <lukasholm@hotmail.com>
2022-11-03 17:05:02 +0100

    added exercises

--- src/Chapter_7/PixelQuantization/Main.java
+++ src/Chapter_7/PixelQuantization/Main.java
@@ -8,22 +8,31 @@ public class Main {
 
     public static void pixelQuantization(){
         int[] pixelArray = new int[200];
+        for (int value:
+             pixelArray) {
+            value = (int) (Math.random() * 200);
+
+        }
         int bound = 20;
         int value = 10;
+        boolean isInRange = false;
         for (int i = 0; i < pixelArray.length; i++) {
 
-            if (!(i <= bound)){
-                bound += 20;
-                value += 20;
+            while (!isInRange) {
+                if (i <= bound) {
+                    bound += 20;
+                    value += 20;
+                } else {
+                    pixelArray[i] = value;
+                    isInRange = true;
+                }
             }
-            pixelArray[i] = value;
-
         }
 
 
         for (int i = 0; i < pixelArray.length; i++) {
             System.out.println("index " + i + "=" + pixelArray[i]);
         }
-
+        
     }
 }
